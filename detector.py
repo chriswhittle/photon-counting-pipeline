Add sharp cutoff to waveform; fix missing factor of arm length in strain->power

detector.py
@@ -54,6 +54,7 @@ def no_photon_prob(f, strain, budget, templates):
     # TODO: this assumes transfer function is flat, i.e. (32)
     # neglects any recycling in IFO.
     Parm = gwinc.ifo.noises.ifo_power(budget.ifo).parm
+    Larm = budget.ifo.Infrastructure.Length
     wavelength = budget.ifo.Laser.Wavelength
     wavenumber = 2 * np.pi / wavelength
     omega = SPEED_OF_LIGHT * wavenumber
@@ -63,7 +64,7 @@ def no_photon_prob(f, strain, budget, templates):
     g = 2 * wavenumber * np.sqrt(Parm / (2*REDUCED_PLANCK*omega))
 
     # output fields
-    output_fields = g * strain
+    output_fields = g * (strain * Larm)
 
     # mean fields in output templates
     # units of sqrt(quanta) TODO: check this

waveform.py
@@ -30,7 +30,7 @@ def inner_product(f, a, b):
     bw = f[1] - f[0]
     return bw * np.sum(a * np.conj(b), axis=0) / (2*np.pi)
 
-def waveform(f, A, f0, bw, phi):
+def waveform(f, A, f0, bw, phi, lim_factor=30):
     """
     Compute and return a Lorentzian wavelet with the following parameters,
     each stored in numpy arrays of equal length (aside from f):
@@ -46,7 +46,12 @@ def waveform(f, A, f0, bw, phi):
         f = f[:, np.newaxis].repeat(A.shape[0], axis=1)
 
     # Lorentzian wavelet # TODO: update waveform
-    wavelet =  np.exp(1j * phi) / (1 + ((f - f0) / (bw/2))**2)
+    wavelet = np.exp(1j * phi) / (1 + ((f - f0) / (bw/2))**2)
+
+    #TODO: add time delay parameter; ~ exp(-2j*pi*f*t0)
+
+    # hard cutoff outside several bandwidths
+    wavelet *= (np.abs(f - f0) < lim_factor * bw)
 
     # time delay to center wavelet at t=0
     bw = f[1] - f[0]